2a7b2c4aeabeb69ac21ff9c498cd74a3ff00fcc5
Joey Kudish <joey@jkudish.com>
2017-03-28 15:38:23 -0700

    initial commit from wp plugin scaffold ✅

---
+++ better-plugin-recommendations.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     Better Plugin Recommendations
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     Better Plugin Recommendations
+ * Author:          jkudish, secretpizzaparty
+ * Author URI:      https://secretpizza.party
+ * Text Domain:     better-plugin-recommendations
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Better_Plugin_Recommendations
+ */
